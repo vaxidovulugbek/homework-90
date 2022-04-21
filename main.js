c07b2730f6fc094ead48f4ef68533a0168972ab0
vakhidovulugbek03 <vulugbekk@gmail.com>
2022-04-21 08:00:52 +0500

    commit

---
+++ main.js
@@ -0,0 +1,70 @@
+
+// 1 masala
+// let a = "js😊"
+// let b = [...a]
+// console.log(b);
+
+// 2 masala
+// function num (a,n) {
+//   console.log(n.toString(a));
+// }
+// num(2,155)
+
+// 3 masala
+// let arr = []
+// function getPrimeNumbersAsArray(n,b,c,d,s) {
+//     if (n < 2){
+//       return 'tub emas';
+//     }
+//     for (let i = 2; i < n; i++){
+//       if (n % i === 0){
+//         return 'tub emas';
+//       }
+//     }
+//     arr.push(n)
+// // ======================================================
+//     if (b < 2){
+//       return 'tub emas';
+//     }
+//     for (let i = 2; i < b; i++){
+//       if (b % i === 0){
+//         return 'tub emas';
+//       }
+//     }
+//     arr.push(b)
+// // ========================================================
+//     if (c < 2){
+//       return 'tub emas';
+//     }
+//     for (let i = 2; i < c; i++){
+//       if (c % i === 0){
+//         return 'tub emas';
+//       }
+//     }
+//     arr.push(c)
+// // ===========================================================
+//     if (d < 2){
+//       return 'tub emas';
+//     }
+//     for (let i = 2; i < d; i++){
+//       if (d % i === 0){
+//         return 'tub emas';
+//       }
+//     }
+//     arr.push(d)
+//   // ==============================================================
+//   if (s < 2){
+//     return 'tub emas';
+//   }
+//   for (let i = 2; i < s; i++){
+//     if (s % i === 0){
+//       return 'tub emas';
+//     }
+//   }
+//   arr.push(s)
+// }
+
+// getPrimeNumbersAsArray(3,2,5,7,12)
+// console.log(arr);
+
+
